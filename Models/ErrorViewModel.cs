e5e5b73b9bcd5aef70d5f898ed06b8b9836ee1af
laboratorio <laboratorio@UPOAULA10609>
2023-10-18 09:17:02 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JuanAlvarez_TallerClase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
